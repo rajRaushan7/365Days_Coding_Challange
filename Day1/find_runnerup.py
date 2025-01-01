e7bb7ab66f450baa858a39b45792cf54f4f52336
Raj Raushan <raj1618192@gmail.com>
2025-01-01 23:59:23 +0530

    Update find_runnerup.py

--- Day1/find_runnerup.py
+++ Day1/find_runnerup.py
@@ -1,19 +1,18 @@
-n = int(input())
-arr = map(int, input().split())
-arr = list(arr)
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    arr = list(arr)
 
-arr.sort()
-print(arr)
+    arr.sort()
 
-arr = set(arr)
-lst = []
-for char in arr:
-    lst.append(char)
-lst.sort()
-print(lst)
+    arr = set(arr)
+    lst = []
+    for char in arr:
+        lst.append(char)
+    lst.sort()
 
-if len(lst) == 1:
-    print(lst[0])
-else:
-    runnerUp = lst[-2]
-    print(runnerUp)
+    if len(lst) == 1:
+        print(lst[0])
+    else:
+       runnerUp = lst[-2]
+       print(runnerUp)
