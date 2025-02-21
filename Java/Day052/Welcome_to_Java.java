83e09fba62a0ea0048240cf5f31395a31725e4b4
rajRaushan7 <raj1618192@gmail.com>
2025-02-21 12:23:18 +0530

    start coding in java

---
+++ Java/Day052/Welcome_to_Java.java
@@ -0,0 +1,21 @@
+/* 
+Welcome to the world of Java! In this challange, we practice printing to stdout.
+The code studs in your editor declare a Solution class and a main method. Complete the main method. 
+Complete the main method by copying the two lines of code below and pasting them inside the body of your main method
+
+Input Format:
+There is no input for this challange
+
+Output Format:
+You must print two lines of output:
+1. Print Hello, World. on the first line.
+2. Print Hello, Java. on the second line.
+*/
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.print("Hello, Java.");
+    }
+}
