fd9cf1e2ebe614a5d392b4f416036a5691915ea5
rajRaushan7 <raj1618192@gmail.com>
2025-02-26 13:07:04 +0530

    add 57th day solution

---
+++ Java/Day057/java_loops2.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class java_loops2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        for (int i = 1; i <=10; i++){
+            System.out.println(N + " " + "x" + " " + i + " " + "=" + " " + N*i);
+        }
+        bufferedReader.close();
+    }
+}
