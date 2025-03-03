b01acb8b99740e3c5a468739a43b0a82d2425dfa
rajRaushan7 <raj1618192@gmail.com>
2025-03-03 20:14:06 +0530

    add 62th day problem

---
+++ Java/Day062/java_end_of_file.java
@@ -0,0 +1,33 @@
+/*
+The challenge here is to read n lines of input until you reach EOF, then number and print all n lines of content.
+
+Hint: Java's Scanner.hasNext() method is helpful for this problem.
+
+Input Format
+
+Read some unknown n lines of input from stdin(System.in) until you reach EOF; each line of input contains a non-empty String.
+
+Output Format
+
+For each line, print the line number, followed by a single space, and then the line content received as input.
+*/
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class java_end_of_file {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int n = 1;
+        while (sc.hasNext()){
+            String line = sc.nextLine();
+            System.out.println(n + " " + line);
+            n = n + 1;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -11,8 +11,8 @@ This repository documents my journey of solving coding problems on HackerRank fo
 
 # Progress Tracker
  	• Start Date: 01/ 01/ 2025
-	• Days Completed: 61/365
-  	• Problems Solved: 73
+	• Days Completed: 62/365
+  	• Problems Solved: 74
 
 # Connect with Me
  	• LinkedIn: https://www.linkedin.com/in/raj-raushan-899973320/
