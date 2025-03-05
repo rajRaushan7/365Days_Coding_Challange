2ec69bf870dde64d5d57970701867b78ceb914dd
rajRaushan7 <raj1618192@gmail.com>
2025-03-05 13:50:16 +0530

    add 64th day solution in java

---
+++ Java/Day064/javaSubString.java
@@ -0,0 +1,21 @@
+/*
+Given a string s, and indices, start and end, print a substring consisting of all characters in the inclusive range from start to end -1.
+*/
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class javaSubString {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        String part = S.substring(start, end);
+        System.out.print(part);
+    }
+}
--- README.md
+++ README.md
@@ -11,8 +11,8 @@ This repository documents my journey of solving coding problems on HackerRank fo
 
 # Progress Tracker
  	• Start Date: 01/ 01/ 2025
-	• Days Completed: 63/365
-  	• Problems Solved: 75
+	• Days Completed: 64/365
+  	• Problems Solved: 76
 
 # Connect with Me
  	• LinkedIn: https://www.linkedin.com/in/raj-raushan-899973320/
