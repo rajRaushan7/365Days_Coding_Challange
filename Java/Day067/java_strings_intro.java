2524c33a2d20deea84fa208a90bb39c86a0e0771
rajRaushan7 <raj1618192@gmail.com>
2025-03-08 19:08:13 +0530

    add 67th day solution in java

---
+++ Java/Day067/java_strings_intro.java
@@ -0,0 +1,44 @@
+/*
+Given two strings of lowercase English letters. A and B, perform the following operations: 
+1. Sum the lengths of A and B.
+2. Determine if A is lexicographically larger than B (i.e. B comes before A in the dictionary).
+3. Capitalize the first letter in A and B and print them on a single line.
+
+Input Format: 
+The first line contains a string A. The second line contains another string B.
+The string are comprised of only lowercase English letter.
+
+Output Format:
+There are three lines of output:
+For the first line, sum of the lengths of A and B.
+For the second line, write Yes if A is lexicographically greater than B otherwise print No instead.
+For the third line, capitalize the first letter in both A and B and print them on a single line, separated by a space.
+*/
+
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        int n1 = A.length() + B.length();
+        int ans = A.compareTo(B);
+        String a = A.substring(0, 1).toUpperCase() + A.substring(1);
+        String b = B.substring(0, 1).toUpperCase() + B.substring(1);
+        System.out.println(n1);
+        if(ans > 0){
+            System.out.println("Yes");
+        } else{
+            System.out.println("No");
+        }
+        System.out.print(a + " " + b);
+    }
+}
+
+
+
--- README.md
+++ README.md
@@ -11,8 +11,8 @@ This repository documents my journey of solving coding problems on HackerRank fo
 
 # Progress Tracker
  	• Start Date: 01/ 01/ 2025
-	• Days Completed: 66/365
-  	• Problems Solved: 78
+	• Days Completed: 67/365
+  	• Problems Solved: 79
 
 # Connect with Me
  	• LinkedIn: https://www.linkedin.com/in/raj-raushan-899973320/
