3a8366fd43e4c62eb1eb0f595c57db9b618bb987
rajRaushan7 <raj1618192@gmail.com>
2025-03-09 18:24:29 +0530

    add 68th day solution in java

---
+++ Java/Day068/java_string_reverse.java
@@ -0,0 +1,28 @@
+/*
+A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward.
+
+Given a string A, print Yes if it is a palindrome, print No otherwise.
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String rev = "";
+        for (int i = A.length() - 1; i >= 0; i--){
+            rev += A.charAt(i);
+        }
+        if (A.equals(rev)){
+            System.out.print("Yes");
+        } else {
+            System.out.print("No");
+        }
+        /* Enter your code here. Print output to STDOUT. */
+        
+    }
+}
--- README.md
+++ README.md
@@ -11,8 +11,8 @@ This repository documents my journey of solving coding problems on HackerRank fo
 
 # Progress Tracker
  	• Start Date: 01/ 01/ 2025
-	• Days Completed: 67/365
-  	• Problems Solved: 79
+	• Days Completed: 68/365
+  	• Problems Solved: 80
 
 # Connect with Me
  	• LinkedIn: https://www.linkedin.com/in/raj-raushan-899973320/
