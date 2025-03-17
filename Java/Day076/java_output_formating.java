2e1c8a81f7e19d1659a1fd3bae2cbb9394d1af42
rajRaushan7 <raj1618192@gmail.com>
2025-03-17 19:57:33 +0530

    add 76th day solution in java

---
+++ Java/Day076/java_output_formating.java
@@ -0,0 +1,24 @@
+/*
+Java's System.out.printf function can be used to print formatted output. The purpose of this exercise is to test your understanding of formatting output using printf.
+To get you started, a portion of the solution is provided form you in the editor:
+you must format and print the input to complete the solution. 
+*/
+
+import java.util.Scanner;
+
+public class java_output_formating {
+
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++){
+                String s1=sc.next();
+                int x=sc.nextInt();
+                String s = s1 + x;
+                //Complete this line
+                System.out.printf("%-14s %03d%n", s1, x);
+            }
+            System.out.println("================================");
+
+    }
+}
--- README.md
+++ README.md
@@ -11,8 +11,8 @@ This repository documents my journey of solving coding problems on HackerRank fo
 
 # Progress Tracker
  	• Start Date: 01/ 01/ 2025
-	• Days Completed: 75/365
-  	• Problems Solved: 86
+	• Days Completed: 76/365
+  	• Problems Solved: 88
 
 # Connect with Me
  	• LinkedIn: https://www.linkedin.com/in/raj-raushan-899973320/
