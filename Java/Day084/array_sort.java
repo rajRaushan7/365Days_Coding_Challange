3e398e8f2f99a9d48cdafe5774d7f68597a27c8c
rajRaushan7 <raj1618192@gmail.com>
2025-03-25 13:49:28 +0530

    add 84th day solution in java

---
+++ Java/Day084/array_sort.java
@@ -0,0 +1,30 @@
+// Sort an array ([5, 4, 1, 3, 2]) using bubble sort method
+
+import java.util.*;
+
+public class array_sort {
+    public static void bubbleSort(int nums[]) {
+        for (int turn = 0; turn < nums.length-1; turn++) {
+            for (int j = 0; j < nums.length-1-turn; j++){
+                if(nums[j] > nums[j+1]){
+                    //swap
+                    int temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printArr(int arr[]){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 1, 3, 2};
+        bubbleSort(arr);
+        printArr(arr);
+    }
+}
