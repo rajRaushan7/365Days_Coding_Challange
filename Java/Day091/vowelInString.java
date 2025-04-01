da9c0a7b2f83c32bc99b91ba1b10680ee3615c22
rajRaushan7 <raj1618192@gmail.com>
2025-04-01 21:36:24 +0530

    add 91st day solution in java

---
+++ Java/Day091/vowelInString.java
@@ -0,0 +1,20 @@
+/*
+Count how many times lowercase vowels occured in a string entered by the user.
+*/
+
+package Java.Day091;
+import java.util.*;
+
+public class vowelInString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int vowel = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u') {
+                vowel += 1;
+            }
+        }
+        System.out.println(vowel);
+    }
+}
