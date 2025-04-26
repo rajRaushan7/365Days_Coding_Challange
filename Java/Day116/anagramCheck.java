143f6a70b74a513d109b885c6c7147c16db9ceeb
rajRaushan7 <raj1618192@gmail.com>
2025-04-26 20:06:21 +0530

    add 116th day solution in java

---
+++ Java/Day116/anagramCheck.java
@@ -0,0 +1,14 @@
+package Java.Day116;
+
+import java.util.Arrays;
+
+public class anagramCheck {
+    public boolean isAnagram(String s, String t) {
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
+        return Arrays.equals(sArray, tArray);
+
+    }
+}
--- README.md
+++ README.md
@@ -11,8 +11,8 @@ This repository documents my journey of solving coding problems on HackerRank fo
 
 # Progress Tracker
  	• Start Date: 01/ 01/ 2025
-	• Days Completed: 115/365
-  	• Problems Solved: 133
+	• Days Completed: 116/365
+  	• Problems Solved: 134
 
 # Connect with Me
  	• LinkedIn: https://www.linkedin.com/in/raj-raushan-899973320/
