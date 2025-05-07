06b22570fcf90487226f7566ffb06d83cebfac9d
rajRaushan7 <raj1618192@gmail.com>
2025-05-07 15:01:34 +0530

    add 127th day solution in java

---
+++ Java/Day127/PairSumEff.java
@@ -0,0 +1,33 @@
+package Java.Day127;
+
+import java.util.ArrayList;
+
+public class PairSumEff {
+
+    public static boolean pairSumEff(ArrayList <Integer> list, int target){
+        int lp = 0;
+        int rp = list.size() - 1;
+        while(lp<rp){
+            if(list.get(lp) + list.get(rp) > target){
+                rp--;
+            } else if(list.get(lp) + list.get(rp) < target){
+                lp++;
+            } else{
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args){
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        int target = 5;
+        System.out.println(pairSumEff(list, target));
+    }
+}
--- README.md
+++ README.md
@@ -11,8 +11,8 @@ This repository documents my journey of solving coding problems on HackerRank fo
 
 # Progress Tracker
  	• Start Date: 01/ 01/ 2025
-	• Days Completed: 126/365
-  	• Problems Solved: 144
+	• Days Completed: 127/365
+  	• Problems Solved: 145
 
 # Connect with Me
  	• LinkedIn: https://www.linkedin.com/in/raj-raushan-899973320/
