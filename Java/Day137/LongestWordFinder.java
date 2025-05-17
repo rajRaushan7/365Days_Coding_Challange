dfa2183b9890ff034d16408bd60a000ac3a2652a
rajRaushan7 <raj1618192@gmail.com>
2025-05-17 20:22:50 +0530

    add 137th day solution in java

---
+++ Java/Day137/LongestWordFinder.java
@@ -0,0 +1,16 @@
+public class LongestWordFinder {
+    public static void main(String[] args) {
+        String sentence = "The quick brown fox, jumped over the lazy dog!";
+        sentence = sentence.replaceAll("[^a-zA-Z ]", "");
+        String[] words = sentence.split("\\s+");
+
+        String longest = "";
+        for (String word : words) {
+            if (word.length() > longest.length()) {
+                longest = word;
+            }
+        }
+
+        System.out.println(longest);
+    }
+}
