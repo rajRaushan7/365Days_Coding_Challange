5e394e2da52748f17de60d48e3a1e751d419dfc6
rajRaushan7 <raj1618192@gmail.com>
2025-06-01 21:28:49 +0530

    add 152nd day solution in java

---
+++ Java/Day152/InnerSolution.java
@@ -0,0 +1,41 @@
+package Java.Day152;
+
+class Solution {
+    // Function to find the majority elements in the array
+    public List<Integer> findMajority(int[] nums) {
+        // Your code goes here.
+       int n = nums.length;
+       int count1 = 0, count2 = 0;
+       Integer candidate1 = null, candidate2 = null;
+       for (int num: nums) {
+           if (candidate1 != null && num == candidate1){
+               count1++;
+           } else if(candidate2 != null && num == candidate2){
+               count2++; 
+           } else if(count1 == 0){
+               candidate1 = num;
+               count1 = 1;
+           } else if(count2 == 0){
+               candidate2 = num;
+               count2 = 1;
+           } else{
+               count1--;
+               count2--;
+           }
+       }
+           
+           count1 = 0;
+           count2 = 0;
+           for (int num : nums) {
+               if(num == candidate1) count1++;
+               else if(num==candidate2) count2++;
+           }
+           
+           List<Integer> result = new ArrayList<>();
+           if(count1>n/3) result.add(candidate1);
+           if(count2>n/3) result.add(candidate2);
+           
+           Collections.sort(result);
+           return result;
+    }
+}
