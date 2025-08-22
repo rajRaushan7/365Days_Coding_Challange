a06c330e4f7324dd09cb98a2883443a9d76563cf
Raj Raushan <raj1618192@gmail.com>
2025-08-22 23:21:47 +0530

    Create CountEvenOdd.java
    add 232nd day solution in java

---
+++ Java/Day232/CountEvenOdd.java
@@ -0,0 +1,17 @@
+public class CountEvenOdd {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        
+        int evenCount = 0, oddCount = 0;
+        
+        for (int num : arr) {
+            if (num % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+        }
+        
+        System.out.println("Even = " + evenCount + ", Odd = " + oddCount);
+    }
+}
