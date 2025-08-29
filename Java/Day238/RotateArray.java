4972532741736178df4dfdd9fd7ecaeedad7b662
rajRaushan7 <raj1618192@gmail.com>
2025-08-29 23:11:35 +0530

    add 238th day solution in java

---
+++ Java/Day238/RotateArray.java
@@ -0,0 +1,30 @@
+package Java.Day238;
+
+public class RotateArray {
+    public static void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+
+        int n = arr.length;
+        k = k % n;
+
+        reverse(arr, 0, n - 1);
+        reverse(arr, 0, k - 1);
+        reverse(arr, k, n - 1);
+
+        System.out.print("Rotated Array: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
