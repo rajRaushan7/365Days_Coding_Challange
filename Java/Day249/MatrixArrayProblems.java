2f9642b0565ef03179ae0eeea7c00c5d2a35bfeb
rajRaushan7 <raj1618192@gmail.com>
2025-09-09 23:28:40 +0530

    add 1 more question in 249th day solution in java

--- Java/Day249/MatrixArrayProblems.java
+++ Java/Day249/MatrixArrayProblems.java
@@ -14,7 +14,6 @@ public class MatrixArrayProblems {
         }
         System.out.println(count);
     }
-
     // Question2. -> find the sum of elements in the second row of a matrix
     public static void sumOf2ndRow(int[][] matrix){
         int sum = 0;
@@ -23,13 +22,35 @@ public class MatrixArrayProblems {
         }
         System.out.println(sum);
     }
-
     // Question3. -> find the transpose of a martix
     public static void transposeOfMatrix(int[][] matrix){
-        
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = i; j < matrix[0].length; j++){
+                if(i < j) {
+                    int temp = matrix[i][j];
+                    matrix[i][j] = matrix[j][i];
+                    matrix[j][i] = temp;
+                }
+            }
+        }
+        for(int i = 0; i<matrix.length; i++){
+            for(int j = 0; j<matrix[0].length; j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println("");
+        }
     }
 
     public static void main(String[] args){
+        int[][] matrix = {
+            {1,2,7},
+            {4,7,6},
+            {7,8,7}
+        };
+        
+        noOfTargets(matrix, 7);
+        sumOf2ndRow(matrix);
+        transposeOfMatrix(matrix);
 
     }
 
