8ae82d3508100361cf93dfa17ce56e5f0f01b9e7
rajRaushan7 <raj1618192@gmail.com>
2025-09-26 22:53:27 +0530

    add 265th day solution in java

---
+++ Python/Day265/reverseWords.py
@@ -0,0 +1,7 @@
+def reverseWords(s: str) -> str:
+    words = s.split() 
+    reversed_words = words[::-1]
+    return " ".join(reversed_words)
+
+print(reverseWords("  the sky   is blue  "))  
+print(reverseWords("hello world"))     
